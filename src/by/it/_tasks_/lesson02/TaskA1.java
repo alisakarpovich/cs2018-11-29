ec70fbdae46c032ff8ccb7dd22f26a358b883c20
alisakarpovich <alisa.karpovich@gmail.com>
2018-12-01 12:06:51 +0300

    Lesson 2 - A

--- src/by/it/_tasks_/lesson02/TaskA1.java
+++ src/by/it/_tasks_/lesson02/TaskA1.java
@@ -9,7 +9,8 @@ Hello world!
 */
 
 class TaskA1 {
-    public static void main(String[] args) {
-
+    public static void main(String[] args)  {
+        System.out.println("Hello world!");
     }
+
 }
--- src/by/it/_tasks_/lesson02/TaskA2.java
+++ src/by/it/_tasks_/lesson02/TaskA2.java
@@ -8,6 +8,18 @@ package by.it._tasks_.lesson02;
 Для автоматической проверки откройте и запустите класс Testing
 */
 
-class TaskA2 {
+public class TaskA2 {
+    private static final String TEXT = "Я начинаю изучать Java";
+    public static void main(String[] args) {
+        System.out.println(TEXT);
 
+        String s = "Я начинаю изучать Java";
+
+        printTextMoreTimes(s, 5);
+    }
+    private static void printTextMoreTimes (String phrase, int count) {
+        for (int i = 1; i < count; i++) {
+            System.out.println(phrase);
+        }
+    }
 }
